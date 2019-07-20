e291993212b02d3779e6049f3f13ed5ad5e5f331
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-07-20 18:20:12 +0900

    fix hello method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    text = 'Hello World!'
+    text = "PARAMS: #{params}"
     render plain: text
   end
 end
