fd9837f7e052f1b5c96dded1bda002faf1150919
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-07-20 17:32:59 +0900

    fix application config

--- config/application.rb
+++ config/application.rb
@@ -13,14 +13,6 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-# Timezone
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
-
-# i18n
-config.i18n.available_locales = [:en, :ja]
-config.i18n.default_locale = :ja
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -37,5 +29,13 @@ module RailsApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Timezone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # i18n
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
   end
 end
